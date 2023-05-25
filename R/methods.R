406a0eb4ec3abee272b54eff17fd65e7d9a93df4
Sandra Siegfried <sandra.siegfried@uzh.ch>
2023-05-25 14:30:02 +0000

    version 0.4-4

--- R/methods.R
+++ R/methods.R
@@ -2,10 +2,18 @@
 mkgrid.cotram <- function(object, n, ...)
   mkgrid(object$count_response, n = n, ...)
 
+# mkgrid.cotram <- function(object, n, smooth = FALSE, ...) {
+#   ret <- mkgrid(object$count_response, ...)
+#   y <- variable.names(object, "response")
+#   if (smooth) ret[[y]] <-
+#       seq(from = min(ret[[y]]), to = max(ret[[y]]), length.out = n)
+#   ret
+# }
+
 as.mlt.cotram <- function(object) {
-  cls <- which(class(object) == "mlt_fit")
-  class(object) <- class(object)[-(1:(cls - 1))]
-  object
+  class(object) <- class(object)[-which(class(object) == "cotram")]
+  # if (object$log_first) warning("The model was fitted to 'response + 1L'.")
+  as.mlt(object)
 }
 
 logLik.cotram <- function(object, parm = coef(as.mlt(object), fixed = FALSE), newdata, ...){
@@ -15,13 +23,13 @@ logLik.cotram <- function(object, parm = coef(as.mlt(object), fixed = FALSE), ne
     if (any(newdata[, response] < 0)) stop("response is non-positive")
     if (!all(newdata[, response] %% 1 == 0)) stop("response is non-integer")
     
-    newdata[, response] <- .count_var(y = newdata[, response], plus_one = as.integer(object$log_first))
+    newdata[, response] <- R_count(y = newdata[, response], plus_one = as.integer(object$log_first))
     return(logLik(as.mlt(object), parm = parm, newdata = newdata, ...))
   }
   logLik(as.mlt(object), parm = parm, ...)
 }
 
-.count_var <- function(y, plus_one) {
+R_count <- function(y, plus_one) {
   ## count response as interval-censored object
   y <- as.integer(y)
   yleft <- y - 1L
